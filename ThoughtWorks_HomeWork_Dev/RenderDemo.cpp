2f2d9348dc19539abfed559600eeb78ed26ab15c
YXP <954222619@qq.com>
2018-08-05 11:01:38 +0800

    gcc,vs 两个版本

--- ThoughtWorks_HomeWork_Dev/RenderDemo.cpp
+++ ThoughtWorks_HomeWork_Dev/RenderDemo.cpp
@@ -5,20 +5,21 @@
 
 int main(int argc, char*argv[])
 {
-	//if(argc < 3 || argc > 4)
-	//{
-	//    printf("Usage: %s <rows> <cols> \"[joints]\"\n",argv[0]);
-	//    printf("Example: %s 4 4 \"-0,1 0,2;0,0 1,0;0,1 1,1;0,2 1,2;1,0 1,1;1,1 1,2;1,1 2,1;1,2 2,2;2,0 2,1\"\n",argv[0]);
-	//    exit(EXIT_FAILURE);
-	//}
+	if(argc < 3 || argc > 4)
+	{
+	    printf("Usage: %s <rows> <cols> \"[joints]\"\n",argv[0]);
+	    printf("Example: %s 4 4 \"-0,1 0,2;0,0 1,0;0,1 1,1;0,2 1,2;1,0 1,1;1,1 1,2;1,1 2,1;1,2 2,2;2,0 2,1\"\n",argv[0]);
+	    exit(EXIT_FAILURE);
+	}
 
-	//int rows = std::atoi(argv[1]), cols = std::atoi(argv[2]);
-	//if (argc > 3)
-		//inpust = std::string(argv[3]);
+	int rows = std::atoi(argv[1]), cols = std::atoi(argv[2]);
+	std::string input = "";
+	if (argc > 3)
+		input = std::string(argv[3]);
 
-	int rows = 4, cols = 4;
+	//int rows = 4, cols = 4;
 	//std::string input = "0,1 0,2;0,0 1,0;0,1 1,1;0,2 1,2;1,0 1,1;1,1 1,2;1,1 2,1;1,2 2,2;2,0 2,1";
-	std::string input = "0,1 0,2";
+	//std::string input = "0,1 0,2";
 
 	auto render = RenderFactory::getRender();
 	std::string maze = render->doRender(rows, cols, input);
